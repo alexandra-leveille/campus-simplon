680b34b4cd64f9bd582fb12367b1244505adfb1d
owlab-io <contact@owlab.io>
2017-03-13 15:03:29 +0100

    ajout version simple calculette et ajout JSetCSS1

---
+++ 7_cssEtJS/utility.js
@@ -0,0 +1,49 @@
+/*global window, document*/
+var log, byId, select, selectAll, parseObserveHandle;
+
+log = function log(val) {
+    "use strict";
+    window.console.log(val);
+};
+
+byId = function byId(id) {
+    "use strict";
+    return document.getElementById(id);
+};
+
+select = function select(sCSS) {
+    "use strict";
+    return document.querySelector(sCSS);
+};
+
+selectAll = function selectAll(sCSS) {
+    "use strict";
+    return document.querySelectorAll(sCSS);
+};
+
+// parse -> (parcourir)
+// observer -> listen type (écouter)
+// handle -> callback function that handles the event
+parseObserveHandle = function parseObserveHandle(list, eventType, handler) {
+    "use strict";
+    var i;
+    try {
+        if (!list || Object.getPrototypeOf(list).toString() !== "[object NodeList]") {
+            throw new Error("first argument must be a nodeList");
+        }
+        if (!eventType || typeof eventType !== "string") {
+            throw new Error("second argument must be a string (event type)");
+        }
+        if (!handler || typeof handler !== "function") {
+            throw new Error("third argument must be a function");
+        }
+
+        for (i = 0; i < list.length; i += 1) {
+            list[i].addEventListener(eventType, handler);
+        }
+
+    } catch (error) {
+        log("error @utility/parseObserve");
+        window.console.error(error);
+    }
+};
