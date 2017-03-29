d2d74e143a62f722b7f581137fcd017a0d49b96f
owlab-io <contact@owlab.io>
2017-03-29 15:16:36 +0200

    ajout de la session php + hssan-clone + palindrome

---
+++ 12_html_forms/traitement.php
@@ -0,0 +1,17 @@
+<?php
+
+
+phpinfo();
+
+//
+// echo "hello world";
+// echo "<br>";
+// echo "votre code PHP ici ... <br>";
+// echo '<a href="index.html">retour formulaire</a>';
+
+// la méthode de notre formulaire est en "post"
+// les données sont donc envoyées vers le serveur
+// dans une variable dite "superglobale" =>
+// nommée $_POST
+// $_POST est un tableau (array) disponible dans toutes
+// les pages de vos programmes PHP !
--- 9_inversionLettres+palindrome/app.js
+++ 9_inversionLettres+palindrome/app.js
@@ -13,7 +13,7 @@ var invertLetters = (function invertLetters() {
     // récupérer chaque lettre et la concaténer avec tmp
     // retourner tmp
     // afficher le mot inversé dans la div#resultat
-    var btn, input, resultat, inverser;
+    var btn, input, resultat, inverser, inverser2, estPalindrome;
 
     window.onload = function start() {
         input = byId("input");
@@ -21,24 +21,35 @@ var invertLetters = (function invertLetters() {
         resultat = byId("resultat");
 
         btn.onclick = function () {
-            var res = inverser(input.value);
-            resultat.innerHTML = res;
+            var palindrome;
+            log(inverser2(input.value));
+            palindrome = estPalindrome(input.value, inverser(input.value));
+            log("est palindrome ?");
+            log(palindrome);
+            resultat.innerHTML = palindrome;
         };
     };
 
     inverser = function inverser(text) {
         var i, arr, tmp = "";
         // on sépare la chaîne en tableau de caractères
-        arr =  text.split("");
+        arr = text.split("");
         // on parcourt le tableau de lettres à l'envers
         for (i = arr.length - 1; i >= 0; i -= 1) {
             tmp += text[i]; // on concatène chaque lettre
         }
-        return tmp;
+        return tmp; // on retourne la string inversée
     };
 
+    inverser2 = function inverser2(text) {
+        // inverser2 produit le même résultat... en plus court !
+        return text.split("").reverse().join("");
+    };
 
-
+    estPalindrome = function estPalindrome(mot, inversion) {
+        return mot === inversion;
+        // retourne un booléen après comparaison des 2 strings
+    };
 
 
 
